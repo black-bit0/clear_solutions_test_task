e67d8a530b5efe3fefbd32c8993eaace5ead0d0e
Roman Dudanets <romario220220@gmail.com>
2024-04-25 10:57:51 +0300

    first commit

---
+++ src/test/java/org/example/clear_solutions/ClearSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.clear_solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolutionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
